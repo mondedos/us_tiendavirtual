6a9fc0ff141f664037ea7be7e4d97d126f53c05d
cuvere <cuvere@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-09 22:30:12 +0000

    paquete de test

---
+++ trunk/negocio-jboss/test/mfis/tiendavirtual/persitencia/AccesoBaseDatos.java
@@ -0,0 +1,21 @@
+package mfis.tiendavirtual.persitencia;
+
+import mfis.tiendavirtual.modelo.dao.DaoGenerico;
+import mfis.tiendavirtual.modelo.dao.SingletonSpringContainer;
+
+import org.testng.annotations.Test;
+
+public class AccesoBaseDatos {
+	
+	
+	@Test
+	public void pruebaConexion(){
+		
+		SingletonSpringContainer contenedor =  SingletonSpringContainer.getInstance();
+		DaoGenerico daoConcreto = (DaoGenerico)contenedor.getBean("DaoGenerico");		
+		
+		System.out.print("Configuracion de Spring-Hibernate OK");
+		
+	}
+
+}
