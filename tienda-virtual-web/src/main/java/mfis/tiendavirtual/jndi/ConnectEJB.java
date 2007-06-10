f846816dd73bd01eb124324d704a0ad1867d1ecf
dariver <dariver@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-10 21:54:22 +0000

    Clases para conectarse remotamente a los EJBS. Solo se implementa a modo de ejemplo ProductoEJB, el resto se haría de la misma forma

---
+++ tienda-virtual-web/src/main/java/mfis/tiendavirtual/jndi/ConnectEJB.java
@@ -0,0 +1,48 @@
+package mfis.tiendavirtual.jndi;
+
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import javax.ejb.EJBHome;
+import javax.ejb.EJBObject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public abstract class ConnectEJB implements EJB{
+
+	public InitialContext initc = null;
+	EJBHome home = null;
+	EJBObject ejb = null;
+
+	String jndi = null;
+
+	public EJBObject getEJB(String jndi) {
+    	Hashtable propiedades = new Hashtable();
+    	propiedades.put("java.naming.factory.initial",
+    	    				"org.jnp.interfaces.NamingContextFactory");
+    	propiedades.put("java.naming.factory.url.pkgs",
+    	    				"org.jboss.naming:org.jnp.interfaces");
+    	propiedades.put("java.naming.provider.url",
+    	    				"localhost");
+
+		this.jndi = jndi;
+		try {
+			if (initc == null) {
+				initc = new InitialContext(propiedades);
+			}
+			home = (EJBHome) initc.lookup(this.jndi);
+
+			ejb = this.getRemoteEJB();
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+		return ejb;
+	}
+
+	public abstract EJBObject getRemoteEJB();
+
+}
