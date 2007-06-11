1cd2bc36fbaae055952711a814547bbf1fea619f
dvazquezgomez <dvazquezgomez@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-11 22:22:11 +0000

    Sin probar.

---
+++ trunk/negocio-jboss/src/mfis/tiendavirtual/modelo/dao/OfertasDAO.java
@@ -0,0 +1,41 @@
+package mfis.tiendavirtual.modelo.dao;
+
+import java.util.List;
+
+import mfis.tiendavirtual.modelo.objetoNegocio.Oferta;
+import mfis.tiendavirtual.modelo.objetoNegocio.Producto;
+
+/**
+ * DAO para el manejo de las ofertas
+ * 
+ * @author Daniel Vázquez
+ *
+ */
+public class OfertasDAO {
+
+	/**
+	 * Metodo para crear una nueva oferta a partir de dos productos
+	 * @param productoA objeto persistente
+	 * @param productoB objeto persistente
+	 */
+	public static void nuevaOferta(Producto productoA, Producto productoB){
+		Oferta o = new Oferta();
+		o.setPrincipal(productoA);
+		o.setSecundario(productoB);
+		DaoGenerico d = new DaoGenerico();
+		d.persistirObjeto(o);
+	}
+
+	/**
+	 * Método para obtener la actual oferta de la tienda
+	 * @return oferta vigente
+	 */
+	public static Oferta getOferta(){
+		DaoGenerico d = new DaoGenerico();
+		List l = null;
+		
+		l = d.obtenerTodos(Oferta.class);
+		
+		return ((Oferta) l.get(0));
+	}
+}
