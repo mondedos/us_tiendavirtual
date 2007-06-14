e878436f94ea3ea4c1d77041f218ecaa9ebee04e
dani.talika <dani.talika@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-14 11:13:26 +0000

    ambiados de nuevo los scripts .bat para entornos MicroSoft!!!!

--- trunk/tienda-virtual-web2/INSTALL_GDATA_CLIENT.bat
+++ trunk/tienda-virtual-web2/INSTALL_GDATA_CLIENT.bat
@@ -1,2 +1,2 @@
-#!/bin/sh
+@echo off
 mvn install:install-file -DgroupId=google -DartifactId=gdata-client -Dversion=1.0.0 -Dpackaging=jar -Dfile=gdata-client-1.0.jar
--- trunk/tienda-virtual-web2/INSTALL_JBOSSALL-CLIENT.bat
+++ trunk/tienda-virtual-web2/INSTALL_JBOSSALL-CLIENT.bat
@@ -1,2 +1,2 @@
-#!/bin/sh
+@echo off
 mvn install:install-file -DgroupId=jboss -DartifactId=jbossall-client -Dversion=4.2.0 -Dpackaging=jar -Dfile=jbossall-client.jar
