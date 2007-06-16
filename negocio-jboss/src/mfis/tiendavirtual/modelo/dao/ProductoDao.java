b035e32b23334cf64f88718ed949ab3ea4878520
dvazquezgomez <dvazquezgomez@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-16 20:44:27 +0000

    Incluido el método de búsqueda de productos con más o menos beneficios generados.

--- negocio-jboss/src/mfis/tiendavirtual/modelo/dao/ProductoDao.java
+++ negocio-jboss/src/mfis/tiendavirtual/modelo/dao/ProductoDao.java
@@ -2,8 +2,6 @@ package mfis.tiendavirtual.modelo.dao;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
