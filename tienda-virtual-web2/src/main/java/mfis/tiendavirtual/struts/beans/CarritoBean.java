2e631ab645cb032f52beb5c18af635d44d91d809
dariver <dariver@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-17 22:16:22 +0000

    Esta clase va a contener lógica necesaria para el correcto funcionamiento del carrito

---
+++ tienda-virtual-web2/src/main/java/mfis/tiendavirtual/struts/beans/CarritoBean.java
@@ -0,0 +1,43 @@
+package mfis.tiendavirtual.struts.beans;
+
+import mfis.tiendavirtual.Carrito;
+import mfis.tiendavirtual.modelo.objetoNegocio.Item;
+import mfis.tiendavirtual.modelo.objetoNegocio.LineaPedido;
+import mfis.tiendavirtual.modelo.objetoNegocio.Producto;
+import struts.WebContext;
+
+public class CarritoBean {
+
+	Carrito carrito = null;
+
+	public CarritoBean(WebContext c) {
+		if(c.getSession("carrito") == null) {
+			this.carrito = new Carrito();
+		} else {
+			this.carrito = (Carrito) c.getSession("carrito");
+		}
+	}
+
+	public void crearLineaPedido(Item i, String categoria, int unidades) {
+		LineaPedido linea = new LineaPedido();
+
+		Float precio = ((Producto)i).getPrecio() ;
+		linea.setCompra(i);
+		linea.setUnidades(unidades);
+		linea.setPrecioUnidad( precio );
+
+		this.carrito.addLineaPedido(linea, precio);
+	}
+
+	public void borrarLineaPedido(int linea) {
+		this.carrito.removeLineaPedido(linea);
+		if(this.carrito.getLineasPedido().isEmpty() ) {
+			this.carrito = null;
+		}
+	}
+
+	public Carrito getCarrito() {
+		return this.carrito;
+	}
+
+}
