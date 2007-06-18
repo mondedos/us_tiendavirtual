10d1022f4589627dd2b45b9dce77c04cf65e6528
dariver <dariver@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-18 22:23:08 +0000

    Se añade parte de la funcionalidad para ver el oferton en portada.

--- trunk/tienda-virtual-web2/src/main/java/mfis/tiendavirtual/struts/actions/StartAction.java
+++ trunk/tienda-virtual-web2/src/main/java/mfis/tiendavirtual/struts/actions/StartAction.java
@@ -1,9 +1,19 @@
 package mfis.tiendavirtual.struts.actions;
 
+import java.rmi.RemoteException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Formatter;
+
+import mfis.tiendavirtual.interfaces.GestionOferta;
+import mfis.tiendavirtual.jndi.EJB;
+import mfis.tiendavirtual.jndi.OfertaEJB;
+import mfis.tiendavirtual.modelo.objetoNegocio.Oferta;
+import mfis.tiendavirtual.modelo.objetoNegocio.Producto;
 
 import struts.MyTilesAction;
 import struts.WebContext;
+import sun.misc.FormattedFloatingDecimal;
 
 
 /**
@@ -37,6 +47,30 @@ public class StartAction extends MyTilesAction {
 //
 //    	c.setRequest("lista", productos);
 
+    	GestionOferta go = (GestionOferta) new OfertaEJB().getEJB(EJB.OFERTAS_JNDI);
+    	Producto prA = null;
+    	Producto prB = null;
+    	try {
+			Oferta o = go.getOferta();
+			prA = o.getPrincipal();
+			prB = o.getSecundario();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		float precioFinal = 0.0f;
+
+		precioFinal = (prA.getPrecio().floatValue() + prB.getPrecio().floatValue());
+
+		//aplicamos el descuento del 10%
+		precioFinal -= (precioFinal * 0.10f);
+
+		String tmp = "" + precioFinal;
+
+		c.setRequest("prA", prA);
+		c.setRequest("prB", prB);
+		c.setRequest("precioFinal", tmp.substring(0, (tmp.indexOf(".")+ 3) ));
     	c.setRequest("lista", new ArrayList());
 
         return MAINPAGE;
