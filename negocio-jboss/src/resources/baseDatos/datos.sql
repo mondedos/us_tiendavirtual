d6a1e8ac31d4c8f137e7ba78026033fec37eb32a
dvazquezgomez <dvazquezgomez@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-20 15:57:06 +0000

    Corregidas descripciones de la base de datos, solucionado el problema de los acentos y eñes así como el de la imagen del frigorífico que no se mostraba.

--- negocio-jboss/src/resources/baseDatos/datos.sql
+++ negocio-jboss/src/resources/baseDatos/datos.sql
@@ -31,48 +31,45 @@
  (30,'ref30');
 
 INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES
- (1,'Sony','KV14CT1','Alto 80 cm - Ancho 70 cm',215.00,15,'Televisor pequeÃ±o, compacto y mono.','Televisor/Sony_KV_14_CT1.jpg'),
- (2,'Fagor','F 2810','Alto 85 cm - Ancho 59 cm',439.00,24,'Lavadora con carga frontal, cuba de acero inoxidable con una capacidad 8kg.','Lavadora/Fagor_F_2810.jpg'),
- (3,'Fagor','3FC 67 NFXD','Alto 185 cm - Ancho 60 cm',699.00,13,'Frigorifico de gran capacidad, con controles de firgorifico y congelador separados','Frigorifico/Fagor_3FC_67_NFXD.jpg'),
- (4,'Zanussi','ZNB 34 NDX','Alto 185 cm - Ancho 60 cm',456.00,10,'Figrorifico con ventilador interno para distribuir el frio, puerta de acero inoxidable y bandejas de cristal.','Frigorifico/Zanussi_ZNB_34_NDX.jpg'),
- (5,'Bosch','KGU 34175 EU','Alto 186 cm - Ancho 60 cm',465.00,6,'Frigorifico con dos motores, de acero inoxidable y bandejas de cristal.','Frigorifico/Bosch_KGU_34175_EU.jpg'),
- (6,'Bosch','KGU 44170 eu','Alto 201cm - Ancho 70cm',1081.58,25,'Frigorifico de acero inoxidable con dos motores y controles frigorifico-congelador separados.','Frigorifico/Bosch_KGU_44170_EU');
+ (1,'Sony','KV14CT1','Alto: 80 cm. - Ancho: 70 cm.',215.00,15,'Televisor pequeño y compacto.','Televisor/Sony_KV_14_CT1.jpg'),
+ (2,'Fagor','F 2810','Alto: 85 cm. - Ancho: 59 cm.',439.00,24,'Lavadora con carga frontal y cuba de acero inoxidable con una capacidad 8 de kg.','Lavadora/Fagor_F_2810.jpg'),
+ (3,'Fagor','3FC 67 NFXD','Alto: 185 cm. - Ancho: 60 cm.',699.00,13,'Frigorífico de gran capacidad, con controles de frigorífico y congelador separados.','Frigorifico/Fagor_3FC_67_NFXD.jpg'),
+ (4,'Zanussi','ZNB 34 NDX','Alto: 185 cm. - Ancho: 60 cm.',456.00,10,'Frigorífico con ventilador interno para distribuir el frío, puerta de acero inoxidable y bandejas de cristal.','Frigorifico/Zanussi_ZNB_34_NDX.jpg'),
+ (5,'Bosch','KGU 34175 EU','Alto: 186 cm. - Ancho: 60 cm.',465.00,6,'Frigorífico con dos motores, de acero inoxidable y bandejas de cristal.','Frigorifico/Bosch_KGU_34175_EU.jpg'),
+ (6,'Bosch','KGU 44170 eu','Alto: 201cm. - Ancho: 70cm.',1081.58,25,'Frigorífico de acero inoxidable con dos motores y controles frigorifico y congelador separados.','Frigorifico/Bosch_KGU_44170_EU.jpg');
+INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES
+ (7,'Philips','21 PT 5402','Alto: 30 cm. - Ancho: 40 cm.',179.52,12,'Televisor con gran calidad de imagen, compacto, estereo y con teletexto.','Televisor/Philips_21_PT_5402.jpg'),
+ (8,'Philips','29 PT 5458','Alto: 50 cm. - Ancho: 60 cm.',499.00,5,'Televisor con gran calidad de imagen y sonido, estereo y con teletexto.','Televisor/Philips_29_PT_5458.jpg'),
+ (9,'Panasonic','TX-26 LXD 65 F','Alto: 90 cm. - Ancho: 110 cm.',824.53,7,'Televisor con sonido envolvente, programable y pantalla LCD.','Televisor/Panasonic_TX_26_LXD_65_F.jpg'),
+ (10,'Panasonic','TX-26LX60F','Alto: 95 cm. - Ancho: 110 cm.',895.00,12,'Televisor con pantalla LCD con gran calidad de imagen y sonido envolvente.','Televisor/Panasonic_TX_26LX60F.jpg'),
+ (11,'Zanussi','ZWG-3125','Alto: 85 cm. - Ancho: 60 cm.',349.00,23,'Lavadora con sistema antidesbordamiento, carga frontal y cuba de plastico de 6 kg. de capacidad.','Lavadora/Zanussi_ZWG_3125.jpg'),
+ (12,'Fagor','F2812','Alto: 85 cm. - Ancho: 59 cm.',499.00,10,'Lavadora con cuba de acero inoxidable y con 8 kg. de capacidad de carga.','Lavadora/Fagor_F_2812.jpg');
 INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES 
- (7,'Philips','21 PT 5402','Alto 30 cm - Ancho 40 cm',179.52,12,'Televisor con gran calidad de imagen, compacto, estereo y con teletexto.','Televisor/Philips_21_PT_5402.jpg'),
- (8,'Philips','29 PT 5458','Alto 50 cm - Ancho 60 cm',499.00,5,'Televisor con gran calidad de imagen y sonido, estereo y con teletexto.','Televisor/Philips_29_PT_5458.jpg'),
- (9,'Panasonic','TX-26 LXD 65 F','Alto 90 cm - Ancho 110 cm',824.53,7,'Televisor con sonido envolvente, programable y pantalla LCD.','Televisor/Panasonic_TX_26_LXD_65_F.jpg'),
- (10,'Panasonic','TX-26LX60F','Alto 95 cm - Ancho 110 cm',895.00,12,'Televisor con pantalla LCD con gran calidad de imagen y sonido envolvente.','Televisor/Panasonic_TX_26LX60F.jpg'),
- (11,'Zanussi','ZWG-3125','Alto 85 cm - Ancho 60 cm',349.00,23,'Lavadora con sistema antidesbordamiento, carga frontal y cuba de plastico de 6 kg de capacidad.','Lavadora/Zanussi_ZWG_3125.jpg'),
- (12,'Fagor','F2812','Alto 85 cm - Ancho 59 cm',499.00,10,'Lavadora con cuba de acero inosidable y con 8 kg de carga.','Lavadora/Fagor_F_2812.jpg');
+ (13,'Whirlpool','FL-5085','Alto: 85 cm. - Ancho: 60 cm.',380.50,7,'Lavadora de carga frontal con una capacidad de 5 kg.','Lavadora/Whirlpool_FL_5085.jpg'),
+ (14,'Whirlpool','AWM-7106','Alto: 85 cm. - Ancho: 60 cm.',320.75,20,'Lavadora con cuba de plástico de 5 kg de capacidad y carga frontal.','Lavadora/Whirlpool_AWM_7106.jpg'),
+ (15,'Panasonic','DVD-LS91','Alto 2.03 in. - Ancho: 9.28 in. - Profundidad 7.28 in.',194.99,12,'Esta impresionante pantalla de 9\" en este reproductor de DVD portátil es ideal para películas en el asiento trasero de su vehículo durante un viaje largo. La entrada dual para auriculares permite que dos personas oigan simultáneamente.','Dvd/Panasonic DVD-LS91.jpg'),
+ (16,'Philips','HR-2094','Alto: 20 cm.',86.20,30,'Pequeña batidora.','PequenosElectrodomesticos/Philips_HR_2094__550219.jpg'),
+ (21,'Taurus','BAPI 600 IX','Tamaño 18 cm. ',28.59,21,'Pequeña batidora.','PequenosElectrodomesticos/Taurus_BAPI_600_IX.jpg'),
+ (22,'Solac','T-222 O2 07798.1','Alto: 9 cm. - Ancho: 34 cm..',23.43,10,'Tostadora','PequenosElectrodomesticos/Solac_T_222_O2_077981.jpg');
 INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES 
- (13,'Whirlpool','FL-5085','Alto 85 cm - Ancho 60 cm',380.50,7,'Lavadora de carga frontal con una capacidad de 5 kg.','Lavadora/Whirlpool_FL_5085.jpg'),
- (14,'Whirlpool','AWM-7106','Alto 85 cm - Ancho 60 cm',320.75,20,'Lavadora con cuba de plÃ¡stico de 5 kg de capacidad y carga frontal.','Lavadora/Whirlpool_AWM_7106.jpg'),
- (15,'Panasonic','DVD-LS91','Altura 2.03 in-Ancho 9.28 in-Profundidad 7.28',194.99,12,'Esta impresionante pantalla de 9\" en este reproductor de DVD portÃ¡til es ideal para pelÃ­culas en el asiento trasero durante un viaje largo. La entrada dual para audÃ­fonos permite que dos personas oigan simultÃ¡neamente.','Dvd/Panasonic DVD-LS91.jpg'),
- (16,'Philips','HR-2094','Altura 20 cm',86.20,30,'PequeÃ±a batidora','PequenosElectrodomesticos/Philips_HR_2094__550219.jpg'),
- (21,'Taurus','BAPI 600 IX','TamaÃ±o 18 cm ',28.59,21,'PequeÃ±a batidora.','PequenosElectrodomesticos/Taurus_BAPI_600_IX.jpg'),
- (22,'Solac','T-222 O2 07798.1','Alto 9 cm - Ancho 34 cm.',23.43,10,'Tostadora','PequenosElectrodomesticos/Solac_T_222_O2_077981.jpg');
+ (23,'Serverin','AT-2201','Alto: 10 cm. - Ancho: 37 cm.',40.12,13,'Tostadora.','PequenosElectrodomesticos/Severin_AT_2201.jpg'),
+ (24,'Fagor','TP-1110','Alto: 4 cm. - Ancho: 20 cm.',35.60,7,'Tostadora.','PequenosElectrodomesticos/Fagor_TP_1110.jpg'),
+ (25,'Kenwood','JE 700','Alto: 24 cm.',228.00,10,'Licuadora.','PequenosElectrodomesticos/Kenwood_JE_700.jpg'),
+ (26,'Samsung','DVD-VR357','Alto: 3 cm. - Ancho 32 cm.',174.54,4,'Esta versátil unidad reproduce tanto discos DVD con VHS, entregando imágenes claras y con una calidad de sonido alta. El Samsung DVD-VR357 incluye un VHS de 4 cabezas Hi-Fi con la claridad visual de una salida Progressive Scan.','Dvd/DVDVR375.jpg'),
+ (27,'Samsung','DVD-HD870','Alto: 3 cm.- Ancho 28 cm.',185.90,6,'El reproductor de DVD de Samsung mejora la señal (1080i/720p) con una salida HDMI, DAC de 14 bits y reproducción de MP3 y WMA.','Dvd/DVD-HD870.jpg'),
+ (28,'Coby','DVD-224','Alto: 3 cm. - Ancho 31 cm.',45.60,9,'Supera tu sistema home theater con el reproductor de Coby DVD-224. Con un diseño delgado, contiene la tecnología Progressive Scan para conseguir la mejor imagen posible. Reproduce tus formatos de discos favoritos incluyendo el formato de audio MP3 para una versatilidad óptima. El acabado plateado hace que complemente cualquier decoración facilmente.','Dvd/DVD224.jpg');
 INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES 
- (23,'Serverin','AT-2201','Alto 10 cm - Ancho 37 cm',40.12,13,'Tostadora','PequenosElectrodomesticos/Severin_AT_2201.jpg'),
- (24,'Fagor','TP-1110','Alto 4 cm - Ancho 20 cm',35.60,7,'Tostadora','PequenosElectrodomesticos/Fagor_TP_1110.jpg'),
- (25,'Kenwood','JE 700','Altura 24 cm',228.00,10,'Licuadora','PequenosElectrodomesticos/Kenwood_JE_700.jpg'),
- (26,'Samsung','DVD-VR357','Altura 3 cm - Anchura 32 cm',174.54,4,'Esta versÃ¡til unidad reproduce tanto discos DVD con casetes VHS, entregando imÃ¡genes claras y con una calidad de sonido alta. El Samsung DVD-VR357 incluye una video casetera de 4 cabezas Hi-Fi con la claridad visual de una salida Progressive scan.','Dvd/DVDVR375.jpg'),
- (27,'Samsung','DVD-HD870','Altura 3 cm- Anchura 28 cm',185.90,6,'El reproductor de DVD de Samsung mejora la seÃ±al (1080i/720P) con una salida HDMI, 14 bit DAC y reproducciÃ³n de WMA/MP3.','Dvd/DVD-HD870.jpg'),
- (28,'Coby','DVD-224','Altura 3 cm - Anchura 31 cm',45.60,9,'Supera tu sistema home theater con el reproductor de alto desempeÃ±o de Coby DVD-224 DVD. Con un diseÃ±o delgado, contiene la tecnologÃ­a Progressive Scan para la mejor imagen posible. Reproduce tus formatos de discos favoritos incluyendo MP3 para una versatilidad Ã³ptima. El acabado plateado hace que complemente cualquier decoraciÃ³n fÃ¡cilmente.','Dvd/DVD224.jpg');
-INSERT INTO `producto` (`ID_ITEM_3`,`VC_MARCA`,`VC_MODELO`,`VC_DIMENSIONES`,`MO_PRECIO`,`IN_GANANCIA`,`VC_DESCRIPCION`,`VC_FOTO`) VALUES 
- (29,'Denon','DVD-3930CI','Altura 4 cm - Anchura 33 cm',156.87,7,'Denon\'s rock-solid DVD-3930CI produces a vivid picture that\'s faithful in every detail. It sports extremely advanced video and audio processing, including the professional-grade Realta HQV chip from Silicon Optix Ã¢â‚¬â€ formerly available from Denon only in their top-of-the-line DVD-5910CI. This powerful chip reduces noise on a pixel-by-pixel basis and upconverts standard DVD signals to 1080p resolution for an unforgettable movie experience with compatible HDTVs. Regardless of whether the original source was film or video, you\'ll enjoy a remarkably crisp and colorful picture, free from blurriness, \"jaggies,\" and other digital artifacts.','Dvd/DVD3930ci.jpg'),
- (30,'Lynx','4FE-23 	 ','Alto: 155 cm - Ancho: 55 cm',319.00,5,'Frigorifico con un solo motor y bandejas de cristal.','Frigorifico/Lynx_4FE_23.jpg');
+ (29,'Denon','DVD-3930CI','Alto: 4 cm. - Ancho: 33 cm.',156.87,7,'El Denon DVD-3930CI reproduce imágenes vivas impresionantes en cada detalle. Incorpora un procesado de audio y video extremadamente avanzado incluyendo el chip de tipo profesional Realta HQV de Silicon Optix. Este potente chip reduce el ruido de cada pixel y genera imágenes 1080p a partir de señales estándar de DVD para conseguir una experiencia inolvidable a la hora de ver sus películas en televisores compatibles con HDTV. Sin importar cual es el origen de la señal conseguirá un realismo y una claridad sin igual libres de toda distorsión.','Dvd/DVD3930ci.jpg'),
+ (30,'Lynx','4FE-23','Alto: 155 cm. - Ancho: 55 cm.',319.00,5,'Frigorífico con un solo motor y bandejas de cristal.','Frigorifico/Lynx_4FE_23.jpg');
 
 
 
 INSERT INTO `dvd` (`ID_ITEM_3`,`VC_FORMATO`,`VC_CONECTORES_IO`) VALUES 
- (15,'Audio CD,CD-R,CD-RW,DVD,DVD Audio,DVD+R,DVD+R DL,DVD+RW,DVD-R,DVD-R DL,DVD-RAM\r\n','Entrada:Digital Optical Audio. \r\nSalida de Audio: Digital Optical,Headphone Out (1/8\" Mini). \r\nSalida de Video: Composite (RCA).'),
- (26,'CD-R, CD-RW, DivX, DVD, DVD+R, DVD+RW, DVD-R, DVD-RAM, DVD-RW','Entrada: A/V (Composite),S-Video.\r\nSalida de Audio: Digital Coaxial, Digital Optical, RCA.\r\nSalida de Video: Component Video, Composite (RCA), HDMI, S-Video.'),
- (27,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDivX,\r\nDVD,\r\nDVD+R,\r\nDVD+RW,\r\nDVD-R,\r\nDVD-RW,\r\nSVCD,\r\nVideo CD (VCD)','Entrada: A/V (Composite).\r\nSalida de Audio: Digital Optical,RCA.\r\nSalida de Video: Component Video, Composite (RCA),nHDMI, S-Video.'),
- (28,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDVD','Salida de Audio: Digital Coaxial, RCA.\r\nSalida de Video: Component Video, Composite (RCA), S-Video.'),
- (29,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDVD,\r\nDVD Audio,\r\nDVD+R,\r\nDVD+RW,\r\nDVD-R,\r\nDVD-RW,\r\nSACD,\r\nVideo CD (VCD)','Entrada: Serial RS-232C - 9 .\r\nSalida de Audio: 5.1-Channel RCA (Decoded), Digital Coaxial, Digital Optical,RCA.\r\nSalida de Video: BNC Component Video, Component Video, Composite (RCA), HDMI, S-Video.');
-
-
-
+ (15,'Audio CD, CD-R, CD-RW, DVD, DVD Audio, DVD+R, DVD+R DL, DVD+RW, DVD-R, DVD-R DL y DVD-RAM\r\n','Entrada: Digital Optical Audio.\r\nSalida de Audio: Digital Optical, Headphone Out (1/8\" Mini).\r\nSalida de vídeo: Composite (RCA).'),
+ (26,'CD-R, CD-RW, DivX, DVD, DVD+R, DVD+RW, DVD-R, DVD-RAM, DVD-RW','Entrada: A/V (Composite) y S-Video.\r\nSalida de Audio: Digital coaxial, Digital Optical y RCA.\r\nSalida de Video: Component Video, Composite (RCA), HDMI y S-Video.'),
+ (27,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDivX,\r\nDVD,\r\nDVD+R,\r\nDVD+RW,\r\nDVD-R,\r\nDVD-RW,\r\nSVCD,\r\nVideo CD (VCD)','Entrada: A/V (Composite).\r\nSalida de Audio: Digital Optical, RCA.\r\nSalida de Video: Component Video, Composite (RCA), HDMI y S-Video.'),
+ (28,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDVD','Salida de Audio: Digital Coaxial, Composite (RCA).\r\nSalida de Video: Component Video, Composite (RCA) y S-Video.'),
+ (29,'Audio CD,\r\nCD-R,\r\nCD-RW,\r\nDVD,\r\nDVD Audio,\r\nDVD+R,\r\nDVD+RW,\r\nDVD-R,\r\nDVD-RW,\r\nSACD,\r\nVideo CD (VCD)','Entrada: Serial RS-232C - 9 .\r\nSalida de Audio: 5.1 Channel RCA, Digital Coaxial, Digital Optical y Composite (RCA).\r\nSalida de Video: BNC Component Video, Component Video, Composite (RCA), HDMI y S-Video.');
 
 INSERT INTO `frigorifico` (`ID_ITEM_3`,`COMBI`,`VC_CLASIFENERGETICA`) VALUES 
  (3,0,'A'),
@@ -81,7 +78,6 @@ INSERT INTO `frigorifico` (`ID_ITEM_3`,`COMBI`,`VC_CLASIFENERGETICA`) VALUES
  (6,0,'A'),
  (30,1,'A');
 
-
 INSERT INTO `lavadora` (`ID_ITEM_3`,`BO_SECADORA`,`VC_CLASIFENERGETICA`) VALUES 
  (2,0,'A+'),
  (11,1,'A+'),
@@ -89,14 +85,11 @@ INSERT INTO `lavadora` (`ID_ITEM_3`,`BO_SECADORA`,`VC_CLASIFENERGETICA`) VALUES
  (13,0,'B'),
  (14,0,'B');
 
-
 INSERT INTO `oferta` (`ID_ITEM_2`,`ID_ITEM_3`,`PRO_ID_ITEM_3`,`ACTUAL`) VALUES 
  (17,16,1,1),
  (18,21,6,0),
  (19,21,10,0),
  (20,16,7,0);
-
-
 
 INSERT INTO `operador` (`ID_OPERADOR`,`VC_LOGIN`) VALUES 
  (1,'danird1982@hotmail.com'),
@@ -104,12 +97,11 @@ INSERT INTO `operador` (`ID_OPERADOR`,`VC_LOGIN`) VALUES
  (3,'dvazquezgomez@gmail.com'),
  (4,'ricardo.prietomendoza@gmail.com');
 
-
 INSERT INTO `pequeno_electrodomestico` (`ID_ITEM_3`,`VC_CARACTERISTICAS`) VALUES 
- (16,'Batidora con vaso de plático y de velocidad variable. Carece de accesori para la minipicadora.'),
+ (16,'Batidora con vaso de plástico y de velocidad variable. Carece de accesorios para la minipicadora.'),
  (21,'Batidora con pie de metal, con envases para batir de diferentes tamaños.'),
- (22,'Totadora con bandeja recogemigas, con botón de parada y sistema calientabollos integrado. '),
- (23,'Tostadora con doble ranura, con botó de parada y sin calientabollos.'),
+ (22,'Tostadora con bandeja recogemigas, con botón de parada y sistema calientabollos integrado.'),
+ (23,'Tostadora con doble ranura, con botón de parada y sin calientabollos.'),
  (24,'Tostadora con bandeja recogemigas, multipan y sin botón de parada.'),
  (25,'Licuadora con dos velocidades y recipiente para pulpa.');
 
