c4e4832b08046a26a2bfcb825978614b13c3e300
dvazquezgomez <dvazquezgomez@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-06-26 08:50:23 +0000

    Eliminada una variable y una importación en OfertaDaoTest que nunca se usaban.

--- negocio-jboss/test/mfis/tiendavirtual/persitencia/OfertaDaoTest.java
+++ negocio-jboss/test/mfis/tiendavirtual/persitencia/OfertaDaoTest.java
@@ -1,6 +1,5 @@
 package mfis.tiendavirtual.persitencia;
 
-import mfis.tiendavirtual.modelo.dao.DaoGenerico;
 import mfis.tiendavirtual.modelo.dao.OfertasDAO;
 import mfis.tiendavirtual.modelo.objetoNegocio.Oferta;
 
@@ -10,18 +9,16 @@ import org.testng.annotations.Test;
 public class OfertaDaoTest {
 	
 	private OfertasDAO ofertaDao;
-	private DaoGenerico daoGenerico;
 	
 	@BeforeClass
 	public void inicializar(){
-		ofertaDao= new OfertasDAO();
-		DaoGenerico daoGenerico= new DaoGenerico();
+		ofertaDao = new OfertasDAO();
 	}
 	
 	@Test
 	public void pruebaObtenerOferta(){
 		
-		Oferta ofertaActual= ofertaDao.obtenerOferta();
+		Oferta ofertaActual = ofertaDao.obtenerOferta();
 		
 		assert ofertaActual.getId().equals(new Long(17)) : "Oferta actual incorrecta";
 	}
