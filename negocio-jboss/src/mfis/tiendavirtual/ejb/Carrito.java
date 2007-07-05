15c882d59bf61ead99b0e3e4510ad0690c42b1cf
dariver <dariver@04135f5a-662f-0410-8e2f-41b93e2c8d1c>
2007-07-05 15:46:59 +0000

    Se corrije removeLineaPedido, para borrar productos y ofertas

--- negocio-jboss/src/mfis/tiendavirtual/ejb/Carrito.java
+++ negocio-jboss/src/mfis/tiendavirtual/ejb/Carrito.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import mfis.tiendavirtual.modelo.objetoNegocio.Item;
 import mfis.tiendavirtual.modelo.objetoNegocio.LineaPedido;
 import mfis.tiendavirtual.modelo.objetoNegocio.Producto;
 
@@ -16,7 +17,7 @@ public class Carrito implements Serializable {
 	private String totalSinIVA = "0";
 	private String totalConIVA = "0";
 	private List<LineaPedido> lineasPedido = null;
-	
+
 	public void addLineaPedido(LineaPedido lp, Float precio) {
 		if(lineasPedido == null) {
 			lineasPedido = new ArrayList<LineaPedido>();
@@ -26,14 +27,14 @@ public class Carrito implements Serializable {
 		this.totalSinIVA = "" + (totalNoIva + 	precio.floatValue() * lp.getUnidades() ) ;
 		this.totalConIVA = "" + (totalNoIva + 	precio.floatValue() * lp.getUnidades() ) * 1.16f ;
 	}
-	
+
 	public void removeLineaPedido(int id) {
 		LineaPedido lp = (LineaPedido) lineasPedido.get(id);
-		Producto p = (Producto) (lp).getCompra();
+		Item p = (lp).getCompra();
 		lineasPedido.remove(id);
 		float totalNoIva = Float.parseFloat(this.totalSinIVA);
-		this.totalSinIVA = "" + (totalNoIva - 	p.getPrecio().floatValue() * lp.getUnidades());
-		this.totalConIVA = "" + (totalNoIva + 	p.getPrecio().floatValue() * lp.getUnidades()) * 1.16f ;
+		this.totalSinIVA = "" + (totalNoIva - 	p.obtenerPrecio().floatValue() * lp.getUnidades());
+		this.totalConIVA = "" + (totalNoIva + 	p.obtenerPrecio().floatValue() * lp.getUnidades()) * 1.16f ;
 	}
 
 	public int getNumProductos() {
@@ -46,15 +47,15 @@ public class Carrito implements Serializable {
 
 		return total;
 	}
-	
+
 	public String getTotalSinIvaFormateado(){
 		return Utilidades.obtenerPrecio(totalSinIVA);
 	}
-	
+
 	public String getTotalConIvaFormateado(){
 		return Utilidades.obtenerPrecio(totalConIVA);
 	}
-	
+
 	public List<LineaPedido> getLineasPedido() {
 		return lineasPedido;
 	}
@@ -73,22 +74,22 @@ public class Carrito implements Serializable {
 	public void setTotalSinIVA(String totalSinIVA) {
 		this.totalSinIVA = totalSinIVA;
 	}
-	
+
 	public String toString() {
 		String res = "";
 		ListIterator li;
 		LineaPedido lP;
-		
+
 		li = lineasPedido.listIterator();
 		while (li.hasNext()) {
 			lP = (LineaPedido) li.next();
-			
+
 			res += lP.toString() + "\n\r";
 		}
 		res += "\r\n=================================================================\r\n";
 		res += "\r\nTotal sin IVA: " + getTotalSinIVA() + "\r\n";
 		res += "Total con IVA: " + getTotalConIVA() + "\r\n";
-		
+
 		return (res);
 	}
 }
